c69cc17ba82ab9502d29fec07cb5cf30bf113591
Aindrela Saha <aindrelasaha22p@gmail.com>
2024-03-13 15:23:06 +0530

    ok

---
+++ 13thmarch.cc
@@ -0,0 +1,22 @@
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//        int s = (n + 1) * n / 2;
+//        return sqrt(s) == int(sqrt(s)) ?  int(sqrt(s)) : -1;
+//     }
+// };
+
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int start = 0;
+        int end = 0;
+        for(int i=n;i>0;i--){
+            start = (i*(i+1))/2;
+            end +=i;
+            if(start == end)    return i;
+        }
+        return -1;
+    }
+};
